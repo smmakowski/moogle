1ed9a4f06a37e6e77d3e060384c132d655aa8ed1
Stephen Makowski <makowski.sm@gmail.com>
2019-12-19 14:25:04 -0600

    static home and search results page

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,13 @@
+from flask import render_template
+from flask import request
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Moogle"
+    return render_template('index.html')
+@app.route('/search')
+def search():
+    q = request.args.get('q')
+    results = []
+    return render_template('search.html', q = q, results = results )
